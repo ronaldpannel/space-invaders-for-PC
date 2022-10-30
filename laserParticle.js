5f5185138eb5188945841df322fd81a5bcc35541
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2022-10-30 10:21:06 +0000

    Add files via upload

--- laserParticle.js
+++ laserParticle.js
@@ -39,8 +39,6 @@ function laserInvaderCollision(opacity) {
         for (let i = 0; i < 10; i++) {
           exParticles.push(new ExParticle(lasers[j].x, lasers[j].y));
         }
-        explosionSound.pause();
-        explosionSound.currentTime = 0;
         explosionSound.play();
         score++;
         invaders.splice(i, 1);
